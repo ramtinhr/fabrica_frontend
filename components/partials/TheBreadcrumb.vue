08b961cdf6877907792e429db01daf44bafb1644
ramtin <ramtin.hrezaei@gmail.com>
2020-12-06 23:18:00 +0330

    create breadcrumb

---
+++ components/partials/TheBreadcrumb.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="breadcrumb">
+    <div class="fabrica-container">
+      <ul>
+        <li v-for="nav in navs">
+          <nuxt-link :to="{ name: `${nav.link}___` }" tag="a">
+            {{ nav.title }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheBreadcrumb',
+  props: {
+    navs: {
+      type: Array,
+      default: null,
+    },
+  },
+}
+</script>
