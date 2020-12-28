04d4391e7b315a7ce4efd92b3ec28c50a5c944cd
ramtin <ramtin.hrezaei@gmail.com>
2020-12-29 02:12:24 +0330

    update

---
+++ pages/404.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="not-found">
+    <section class="error-container">
+      <span class="four"><span class="screen-reader-text">4</span></span>
+      <span class="zero"><span class="screen-reader-text">0</span></span>
+      <span class="four"><span class="screen-reader-text">4</span></span>
+    </section>
+    <div class="link-container">
+      <nuxt-link
+        :to="{ name: 'index___' + $cookies.get('lang') }"
+        class="more-link"
+      >
+        {{ $t('returnToMainPage') }}
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+}
+</script>
+
+<style scoped></style>
