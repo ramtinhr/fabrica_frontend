3b70c02a18cd88e8e41efc2343ce69ea30540690
ramtin <ramtin.hrezaei@gmail.com>
2020-12-30 15:08:40 +0330

    fix

--- plugins/application.js
+++ plugins/application.js
@@ -3,7 +3,7 @@ import CookieParser from 'cookieparser'
 export default ({ $axios, req }) => {
   if (process.client) {
     $axios.setToken(localStorage.getItem('access_token'), 'Bearer')
-  } else if (process.server && req.headers.cookie) {
+  } else {
     const cookies = CookieParser.parse(req.headers.cookie)
     const token = cookies.token
     $axios.setToken(token, 'Bearer')
