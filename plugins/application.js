6a86278860a3d6da0e34bcfa37835772ab01f6b7
ramtin <ramtin.hrezaei@gmail.com>
2020-12-30 15:24:22 +0330

    fix

--- plugins/application.js
+++ plugins/application.js
@@ -3,7 +3,7 @@ import CookieParser from 'cookieparser'
 export default ({ $axios, req }) => {
   if (process.client) {
     $axios.setToken(localStorage.getItem('access_token'), 'Bearer')
-  } else {
+  } else if (req.header.type === 'String' && process.server) {
     const cookies = CookieParser.parse(req.headers.cookie || '')
     if (cookies.token) {
       const token = cookies.token
