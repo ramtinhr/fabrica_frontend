30292998abfd61b6fc19c4b2a20a02f1b21804a0
ramtin <ramtin.hrezaei@gmail.com>
2020-12-30 15:19:47 +0330

    fix

--- plugins/application.js
+++ plugins/application.js
@@ -4,8 +4,10 @@ export default ({ $axios, req }) => {
   if (process.client) {
     $axios.setToken(localStorage.getItem('access_token'), 'Bearer')
   } else {
-    const cookies = CookieParser.parse(req.headers.cookie)
-    const token = cookies.token
-    $axios.setToken(token, 'Bearer')
+    const cookies = CookieParser.parse(req.headers.cookie || '')
+    if (cookies.token) {
+      const token = cookies.token
+      $axios.setToken(token, 'Bearer')
+    }
   }
 }
