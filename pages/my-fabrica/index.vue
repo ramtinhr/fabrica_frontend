c5a6cab5b33a110696241181ab92b55b14370917
ramtin <ramtin.hrezaei@gmail.com>
2020-12-30 16:19:42 +0330

    fix

--- pages/my-fabrica/index.vue
+++ pages/my-fabrica/index.vue
@@ -1,10 +1,5 @@
 <template>
-  <div v-if="isLoading" class="my-fabrica__loading">
-    <TheLoading :color="'#707070'" :size="'60px'" />
-  </div>
-  <div v-else>
     <MyFabricaProfile />
-  </div>
 </template>
 
 <script>
@@ -14,17 +9,6 @@ export default {
   middleware: 'auth',
   async asyncData({ store }) {
     await store.dispatch('auth/fetchMe')
-  },
-  data() {
-    return {
-      isLoading: true,
-    }
-  },
-  created() {
-    this.isLoading = true
-  },
-  beforeMount() {
-    this.isLoading = false
   },
 }
 </script>
